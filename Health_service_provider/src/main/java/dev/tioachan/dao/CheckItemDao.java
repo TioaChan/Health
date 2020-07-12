ba4e905b08b1ec29680b39f11c42d55c50ee2cd7
TioaChan <xiaozhidiguo@gmail.com>
2020-07-12 18:48:27 +0800

    checkitem:revert query item on page load

--- Health_service_provider/src/main/java/dev/tioachan/dao/CheckItemDao.java
+++ Health_service_provider/src/main/java/dev/tioachan/dao/CheckItemDao.java
@@ -2,7 +2,9 @@ package dev.tioachan.dao;
 
 import com.github.pagehelper.Page;
 import dev.tioachan.domain.CheckItem;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CheckItemDao {
 	void add(CheckItem checkItem);
 
